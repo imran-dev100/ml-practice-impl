2ea45da0abf5354f9712ab33d9e3e23e6feb8a3e
imran-dev100 <iagro7189@gmail.com>
2024-02-18 13:21:46 +0530

    Gradient Descent

---
+++ gradient-descent/gradient_descent.py
@@ -0,0 +1,368 @@
+import csv, math, copy
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MaxNLocator
+
+#-------------------------------------------------#
+#              COMPUTE COST FUNCTION              #
+#-------------------------------------------------#
+
+def compute_cost(x, y, w, b): 
+    """
+    Computes the cost function for linear regression.
+    
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    
+    Returns
+        total_cost (float): The cost of using w,b as the parameters for linear regression
+               to fit the data points in x and y
+    """
+    # number of training examples
+    m = x.shape[0] 
+    
+    cost_sum = 0 
+    for i in range(m): 
+        f_wb = w * x[i] + b   
+        cost = (f_wb - y[i]) ** 2  
+        cost_sum = cost_sum + cost  
+    total_cost = (1 / (2 * m)) * cost_sum  
+
+    return total_cost
+
+
+#-------------------------------------------------#
+#                HELPER METHODS                   #
+#-------------------------------------------------#
+
+##########################################################
+# Plotting Routines
+##########################################################
+
+def plt_house_x(X, y,f_wb=None, ax=None):
+    ''' plot house with aXis '''
+    if not ax:
+        fig, ax = plt.subplots(1,1)
+    ax.scatter(X, y, marker='x', c='r', label="Actual Value")
+
+    ax.set_title("Housing Prices")
+    ax.set_ylabel('Price (in 1000s of dollars)')
+    ax.set_xlabel(f'Size (1000 sqft)')
+    if f_wb is not None:
+        ax.plot(X, f_wb,  c='blue', label="Our Prediction")
+    ax.legend()
+
+def inbounds(a,b,xlim,ylim):
+    xlow,xhigh = xlim
+    ylow,yhigh = ylim
+    ax, ay = a
+    bx, by = b
+    if (ax > xlow and ax < xhigh) and (bx > xlow and bx < xhigh) \
+        and (ay > ylow and ay < yhigh) and (by > ylow and by < yhigh):
+        return True
+    return False
+
+def plt_contour_wgrad(x, y, hist, ax, w_range=[-100, 500, 5], b_range=[-500, 500, 5],
+                contours = [0.1,50,1000,5000,10000,25000,50000],
+                      resolution=5, w_final=200, b_final=100,step=10 ):
+    b0,w0 = np.meshgrid(np.arange(*b_range),np.arange(*w_range))
+    z=np.zeros_like(b0)
+    for i in range(w0.shape[0]):
+        for j in range(w0.shape[1]):
+            z[i][j] = compute_cost(x, y, w0[i][j], b0[i][j] )
+
+    CS = ax.contour(w0, b0, z, contours, linewidths=2,
+                   colors=['blue', 'orange', 'darkred', 'magenta', 'purple'])
+    ax.clabel(CS, inline=1, fmt='%1.0f', fontsize=10)
+    ax.set_xlabel("w");  ax.set_ylabel("b")
+    ax.set_title('Contour plot of cost J(w,b), vs b,w with path of gradient descent')
+    w = w_final; b=b_final
+    ax.hlines(b, ax.get_xlim()[0],w, lw=2, color='purple', ls='dotted')
+    ax.vlines(w, ax.get_ylim()[0],b, lw=2, color='purple', ls='dotted')
+
+    base = hist[0]
+    for point in hist[0::step]:
+        edist = np.sqrt((base[0] - point[0])**2 + (base[1] - point[1])**2)
+        if(edist > resolution or point==hist[-1]):
+            if inbounds(point,base, ax.get_xlim(),ax.get_ylim()):
+                plt.annotate('', xy=point, xytext=base,xycoords='data',
+                         arrowprops={'arrowstyle': '->', 'color': 'r', 'lw': 3},
+                         va='center', ha='center')
+            base=point
+    return
+
+def plt_divergence(p_hist, J_hist, x_train,y_train):
+
+    x=np.zeros(len(p_hist))
+    y=np.zeros(len(p_hist))
+    v=np.zeros(len(p_hist))
+    for i in range(len(p_hist)):
+        x[i] = p_hist[i][0]
+        y[i] = p_hist[i][1]
+        v[i] = J_hist[i]
+
+    fig = plt.figure(figsize=(12,5))
+    plt.subplots_adjust( wspace=0 )
+    gs = fig.add_gridspec(1, 5)
+    fig.suptitle(f"Cost escalates when learning rate is too large")
+    #===============
+    #  First subplot
+    #===============
+    ax = fig.add_subplot(gs[:2], )
+
+    # Print w vs cost to see minimum
+    fix_b = 100
+    w_array = np.arange(-7000, 7000, 1000)
+    cost = np.zeros_like(w_array)
+
+    for i in range(len(w_array)):
+        tmp_w = w_array[i]
+        cost[i] = compute_cost(x_train, y_train, tmp_w, fix_b)
+
+    ax.plot(w_array, cost)
+    ax.plot(x,v, c='magenta')
+    ax.set_title("Cost vs w, b set to 100")
+    ax.set_ylabel('Cost')
+    ax.set_xlabel('w')
+    ax.xaxis.set_major_locator(MaxNLocator(2))
+
+    #===============
+    # Second Subplot
+    #===============
+
+    tmp_b,tmp_w = np.meshgrid(np.arange(-35000, 35000, 500),np.arange(-70000, 70000, 500))
+    z=np.zeros_like(tmp_b)
+    for i in range(tmp_w.shape[0]):
+        for j in range(tmp_w.shape[1]):
+            z[i][j] = compute_cost(x_train, y_train, tmp_w[i][j], tmp_b[i][j] )
+
+    ax = fig.add_subplot(gs[2:], projection='3d')
+    ax.plot_surface(tmp_w, tmp_b, z,  alpha=0.3, color='blue')
+    ax.xaxis.set_major_locator(MaxNLocator(2))
+    ax.yaxis.set_major_locator(MaxNLocator(2))
+
+    ax.set_xlabel('w', fontsize=16)
+    ax.set_ylabel('b', fontsize=16)
+    ax.set_zlabel('\ncost', fontsize=16)
+    plt.title('Cost vs (b, w)')
+    # Customize the view angle
+    ax.view_init(elev=20., azim=-65)
+    ax.plot(x, y, v,c='magenta')
+
+    return
+
+# draw derivative line
+# y = m*(x - x1) + y1
+def add_line(dj_dx, x1, y1, d, ax):
+    x = np.linspace(x1-d, x1+d,50)
+    y = dj_dx*(x - x1) + y1
+    ax.scatter(x1, y1, color='blue', s=50)
+    ax.plot(x, y, '--', c='darkred',zorder=10, linewidth = 1)
+    xoff = 30 if x1 == 200 else 10
+    ax.annotate(r"$\frac{\partial J}{\partial w}$ =%d" % dj_dx, fontsize=14,
+                xy=(x1, y1), xycoords='data',
+            xytext=(xoff, 10), textcoords='offset points',
+            arrowprops=dict(arrowstyle="->"),
+            horizontalalignment='left', verticalalignment='top')
+
+
+
+def plt_gradients(x_train,y_train, f_compute_cost, f_compute_gradient):
+    #===============
+    #  First subplot
+    #===============
+    fig,ax = plt.subplots(1,2,figsize=(12,4))
+        
+    # Print w vs cost to see minimum
+    fix_b = 100
+    w_array = np.linspace(-100, 500, 50)
+    w_array = np.linspace(0, 400, 50)
+    cost = np.zeros_like(w_array)
+
+    for i in range(len(w_array)):
+        tmp_w = w_array[i]
+        cost[i] = f_compute_cost(x_train, y_train, tmp_w, fix_b)
+    ax[0].plot(w_array, cost,linewidth=1)
+    ax[0].set_title("Cost vs w, with gradient; b set to 100")
+    ax[0].set_ylabel('Cost')
+    ax[0].set_xlabel('w')
+
+    # plot lines for fixed b=100
+    for tmp_w in [100,200,300]:
+        fix_b = 100
+        dj_dw,dj_db = f_compute_gradient(x_train, y_train, tmp_w, fix_b )
+        j = f_compute_cost(x_train, y_train, tmp_w, fix_b)
+        add_line(dj_dw, tmp_w, j, 30, ax[0])
+
+    #===============
+    # Second Subplot
+    #===============
+
+    tmp_b,tmp_w = np.meshgrid(np.linspace(-200, 200, 10), np.linspace(-100, 600, 10))
+    U = np.zeros_like(tmp_w)
+    V = np.zeros_like(tmp_b)
+    for i in range(tmp_w.shape[0]):
+        for j in range(tmp_w.shape[1]):
+            U[i][j], V[i][j] = f_compute_gradient(x_train, y_train, tmp_w[i][j], tmp_b[i][j] )
+    X = tmp_w
+    Y = tmp_b
+    n=-2
+    color_array = np.sqrt(((V-n)/2)**2 + ((U-n)/2)**2)
+
+    ax[1].set_title('Gradient shown in quiver plot')
+    Q = ax[1].quiver(X, Y, U, V, color_array, units='width', )
+    ax[1].quiverkey(Q, 0.9, 0.9, 2, r'$2 \frac{m}{s}$', labelpos='E',coordinates='figure')
+    ax[1].set_xlabel("w"); ax[1].set_ylabel("b")
+
+#-------------------------------------------------#
+#                  COMPUTE GRADIENT               #
+#-------------------------------------------------#
+
+def compute_gradient(x, y, w, b): 
+    """
+    Computes the gradient for linear regression 
+    Args:
+      x (ndarray (m,)): Data, m examples 
+      y (ndarray (m,)): target values
+      w,b (scalar)    : model parameters  
+    Returns
+      dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
+      dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
+     """
+    
+    # Number of training examples
+    m = x.shape[0]    
+    dj_dw = 0
+    dj_db = 0
+    
+    for i in range(m):  
+        f_wb = w * x[i] + b 
+        dj_dw_i = (f_wb - y[i]) * x[i] 
+        dj_db_i = f_wb - y[i] 
+        dj_db += dj_db_i
+        dj_dw += dj_dw_i 
+    dj_dw = dj_dw / m 
+    dj_db = dj_db / m 
+        
+    return dj_dw, dj_db
+
+#-------------------------------------------------#
+#           COMPUTE GRADIENT DESCENT              #
+#-------------------------------------------------#
+
+def gradient_descent(x, y, w_in, b_in, alpha, num_iters, cost_function, gradient_function): 
+    """
+    Performs gradient descent to fit w,b. Updates w,b by taking 
+    num_iters gradient steps with learning rate alpha
+    
+    Args:
+      x (ndarray (m,))  : Data, m examples 
+      y (ndarray (m,))  : target values
+      w_in,b_in (scalar): initial values of model parameters  
+      alpha (float):     Learning rate
+      num_iters (int):   number of iterations to run gradient descent
+      cost_function:     function to call to produce cost
+      gradient_function: function to call to produce gradient
+      
+    Returns:
+      w (scalar): Updated value of parameter after running gradient descent
+      b (scalar): Updated value of parameter after running gradient descent
+      J_history (List): History of cost values
+      p_history (list): History of parameters [w,b] 
+      """
+    
+    # An array to store cost J and w's at each iteration primarily for graphing later
+    J_history = []
+    p_history = []
+    b = b_in
+    w = w_in
+    
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters using gradient_function
+        dj_dw, dj_db = gradient_function(x, y, w , b)     
+
+        # Update Parameters using equation (3) above
+        b = b - alpha * dj_db                            
+        w = w - alpha * dj_dw                            
+
+        # Save cost J at each iteration
+        if i<100000:      # prevent resource exhaustion 
+            J_history.append( cost_function(x, y, w , b))
+            p_history.append([w,b])
+        # Print cost every at intervals 10 times or as many iterations if < 10
+        if i% math.ceil(num_iters/10) == 0:
+            print(f"Iteration {i:4}: Cost {J_history[-1]:.3f} ",
+                  f"dj_dw: {dj_dw:.3f}, dj_db: {dj_db:.3f}  ",
+                  f"w: {w:.3f}, b:{b:.3f}")
+ 
+    return w, b, J_history, p_history #return w and J,w history for graphing
+
+#-------------------------------------------------#
+#                  SOURCE CODE                    #
+#-------------------------------------------------#
+
+file='/Volumes/ExternalSSD/ML-Specialization/jupyter-workspace/ml-practice-impl/sample-data/Housing-2.csv'
+
+x_train = np.array([]) 
+y_train = np.array([]) 
+
+with open(file, mode ='r')as file:
+  csvFile = csv.reader(file)
+  header = next(csvFile) # skipping headers
+  
+  for line in csvFile:
+        x_train = np.append(x_train, float(line[1])/1000)
+        y_train = np.append(y_train, float(line[0])/1000)
+
+x_train = np.array(x_train, dtype=np.float64) # Area of the house in 1000 Sq.Ft.
+y_train = np.array(y_train, dtype=np.float64) # Price of the house in 1000 USD
+
+
+plt_gradients(x_train,y_train, compute_cost, compute_gradient)
+plt.show()
+
+
+
+# initialize parameters
+w_init = 1000
+b_init = 100
+# some gradient descent settings
+iterations = 10000
+tmp_alpha = 1.0e-2
+# run gradient descent
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train ,y_train, w_init, b_init, tmp_alpha, 
+                                                    iterations, compute_cost, compute_gradient)
+print(f"(w,b) found by gradient descent: ({w_final}, {b_final})")
+
+# plot cost versus iteration  
+fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True, figsize=(12,4))
+ax1.plot(J_hist[:100])
+ax2.plot(1000 + np.arange(len(J_hist[1000:])), J_hist[1000:])
+ax1.set_title("Cost vs. iteration(start)");  ax2.set_title("Cost vs. iteration (end)")
+ax1.set_ylabel('Cost')            ;  ax2.set_ylabel('Cost') 
+ax1.set_xlabel('iteration step')  ;  ax2.set_xlabel('iteration step') 
+plt.show()
+
+print(f"1000 sqft house prediction {w_final*1.0 + b_final:0.1f} Thousand dollars")
+print(f"1200 sqft house prediction {w_final*1.2 + b_final:0.1f} Thousand dollars")
+print(f"2000 sqft house prediction {w_final*2.0 + b_final:0.1f} Thousand dollars")
+
+
+fig, ax = plt.subplots(1,1, figsize=(12, 6))
+plt_contour_wgrad(x_train, y_train, p_hist, ax)
+fig, ax = plt.subplots(1,1, figsize=(12, 4))
+plt_contour_wgrad(x_train, y_train, p_hist, ax, w_range=[180, 220, 0.5], b_range=[80, 120, 0.5],
+            contours=[1,5,10,20],resolution=0.5)
+# initialize parameters
+w_init = 0
+b_init = 0
+# set alpha to a large value
+iterations = 10
+tmp_alpha = 8.0e-1
+# run gradient descent
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train ,y_train, w_init, b_init, tmp_alpha, 
+                                                    iterations, compute_cost, compute_gradient)
+plt_divergence(p_hist, J_hist,x_train, y_train)
+plt.show()
