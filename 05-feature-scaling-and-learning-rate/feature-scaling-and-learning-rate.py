19e1949494b35b3e3917049b07af67b17b8e5162
imran-dev100 <iagro7189@gmail.com>
2024-03-03 19:32:53 +0530

    Adding implementation of feature scaling

--- 05-feature-scaling-and-learning-rate/feature-scaling-and-learning-rate.py
+++ 05-feature-scaling-and-learning-rate/feature-scaling-and-learning-rate.py
@@ -2,6 +2,7 @@ import csv
 import numpy as np
 import copy, math
 import matplotlib.pyplot as plt
+from scipy.stats import norm
 # from lab_utils_multi import  load_house_data, run_gradient_descent 
 # from lab_utils_multi import  norm_plot, plt_equal_scale, plot_cost_i_w
 # from lab_utils_common import dlc
@@ -174,6 +175,100 @@ def plot_cost_i_w(X,y,hist):
     ax[1].plot(ws[:,0],hist["cost"])
     plt.show()
 
+def norm_plot(ax, data):
+    scale = (np.max(data) - np.min(data))*0.2
+    x = np.linspace(np.min(data)-scale,np.max(data)+scale,50)
+    _,bins, _ = ax.hist(data, x, color="xkcd:azure")
+    #ax.set_ylabel("Count")
+    
+    mu = np.mean(data); 
+    std = np.std(data); 
+    dist = norm.pdf(bins, loc=mu, scale = std)
+    
+    axr = ax.twinx()
+    axr.plot(bins,dist, color = "orangered", lw=2)
+    axr.set_ylim(bottom=0)
+    axr.axis('off')
+
+def zscore_normalize_features(X):
+    """
+    computes  X, zcore normalized by column
+    
+    Args:
+      X (ndarray (m,n))     : input data, m examples, n features
+      
+    Returns:
+      X_norm (ndarray (m,n)): input normalized by column
+      mu (ndarray (n,))     : mean of each feature
+      sigma (ndarray (n,))  : standard deviation of each feature
+    """
+    # find the mean of each column/feature
+    mu     = np.mean(X, axis=0)                 # mu will have shape (n,)
+    # find the standard deviation of each column/feature
+    sigma  = np.std(X, axis=0)                  # sigma will have shape (n,)
+    # element-wise, subtract mu for that column from each example, divide by std for that column
+    X_norm = (X - mu) / sigma      
+
+    return (X_norm, mu, sigma)
+ 
+#check our work
+#from sklearn.preprocessing import scale
+#scale(X_orig, axis=0, with_mean=True, with_std=True, copy=True)
+
+
+def plt_equal_scale(X_train, X_norm, y_train):
+    fig,ax = plt.subplots(1,2,figsize=(12,5))
+    prange = [
+              [ 0.238-0.045, 0.238+0.045,  50],
+              [-25.77326319-0.045, -25.77326319+0.045, 50],
+              [-50000, 0,      50],
+              [-1500,  0,      50],
+              [0, 200000, 50]]
+    w_best = np.array([0.23844318, -25.77326319, -58.11084634,  -1.57727192])
+    b_best = 235
+    plt_contour_multi(X_train, y_train, w_best, b_best, ax[0], prange, 0, 1, 
+                      title='Unnormalized, J(w,b), vs w[0],w[1]',
+                      xlabel= "w[0] (size(sqft))", ylabel="w[1] (# bedrooms)")
+    #
+    w_best = np.array([111.1972, -16.75480051, -28.51530411, -37.17305735])
+    b_best = 376.949151515151
+    prange = [[ 111-50, 111+50,   75],
+              [-16.75-50,-16.75+50, 75],
+              [-28.5-8, -28.5+8,  50],
+              [-37.1-16,-37.1+16, 50],
+              [376-150, 376+150, 50]]
+    plt_contour_multi(X_norm, y_train, w_best, b_best, ax[1], prange, 0, 1, 
+                      title='Normalized, J(w,b), vs w[0],w[1]',
+                      xlabel= "w[0] (normalized size(sqft))", ylabel="w[1] (normalized # bedrooms)")
+    fig.suptitle("Cost contour with equal scale", fontsize=18)
+    #plt.tight_layout(rect=(0,0,1.05,1.05))
+    fig.tight_layout(rect=(0,0,1,0.95))
+    plt.show()
+
+# plots p1 vs p2. Prange is an array of entries [min, max, steps]. In feature scaling lab.
+def plt_contour_multi(x, y, w, b, ax, prange, p1, p2, title="", xlabel="", ylabel=""): 
+    contours = [1e2, 2e2,3e2,4e2, 5e2, 6e2, 7e2,8e2,1e3, 1.25e3,1.5e3, 1e4, 1e5, 1e6, 1e7]
+    px,py = np.meshgrid(np.linspace(*(prange[p1])),np.linspace(*(prange[p2])))
+    z=np.zeros_like(px)
+    n,_ = px.shape
+    for i in range(px.shape[0]):
+        for j in range(px.shape[1]):
+            w_ij = w
+            b_ij = b
+            if p1 <= 3: w_ij[p1] = px[i,j]
+            if p1 == 4: b_ij = px[i,j]
+            if p2 <= 3: w_ij[p2] = py[i,j]
+            if p2 == 4: b_ij = py[i,j]
+                
+            z[i][j] = compute_cost(x, y, w_ij, b_ij )
+    CS = ax.contour(px, py, z, contours, linewidths=2,
+                   colors=["blue", "orange", "darkred", "magenta", "purple"]) 
+    ax.clabel(CS, inline=1, fmt='%1.2e', fontsize=10)
+    ax.set_xlabel(xlabel);  ax.set_ylabel(ylabel)
+    ax.set_title(title, fontsize=14)
+
+
+
 ##########################################################
 ##########################################################
 ###################### MAIN EXECUTION ####################
@@ -211,3 +306,111 @@ plot_cost_i_w(X_train, y_train, hist)
 _,_,hist = run_gradient_descent(X_train, y_train, 10, alpha = 1e-7)
 
 plot_cost_i_w(X_train,y_train,hist)
+
+
+mu     = np.mean(X_train,axis=0)   
+sigma  = np.std(X_train,axis=0) 
+X_mean = (X_train - mu)
+X_norm = (X_train - mu)/sigma      
+
+fig,ax=plt.subplots(1, 3, figsize=(12, 3))
+ax[0].scatter(X_train[:,0], X_train[:,3])
+ax[0].set_xlabel(X_features[0]); ax[0].set_ylabel(X_features[3]);
+ax[0].set_title("unnormalized")
+ax[0].axis('equal')
+
+ax[1].scatter(X_mean[:,0], X_mean[:,3])
+ax[1].set_xlabel(X_features[0]); ax[0].set_ylabel(X_features[3]);
+ax[1].set_title(r"X - $\mu$")
+ax[1].axis('equal')
+
+ax[2].scatter(X_norm[:,0], X_norm[:,3])
+ax[2].set_xlabel(X_features[0]); ax[0].set_ylabel(X_features[3]);
+ax[2].set_title(r"Z-score normalized")
+ax[2].axis('equal')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+fig.suptitle("distribution of features before, during, after normalization")
+plt.show()
+
+# normalize the original features
+X_norm, X_mu, X_sigma = zscore_normalize_features(X_train)
+print(f"X_mu = {X_mu}, \nX_sigma = {X_sigma}")
+print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_train,axis=0)}")   
+print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
+
+fig,ax=plt.subplots(1, 4, figsize=(12, 3))
+for i in range(len(ax)):
+    norm_plot(ax[i],X_train[:,i],)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("count");
+fig.suptitle("distribution of features before normalization")
+plt.show()
+fig,ax=plt.subplots(1,4,figsize=(12,3))
+for i in range(len(ax)):
+    norm_plot(ax[i],X_norm[:,i],)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("count"); 
+fig.suptitle("distribution of features after normalization")
+
+plt.show()
+
+w_norm, b_norm, hist = run_gradient_descent(X_norm, y_train, 1000, 1.0e-1, )
+
+# First, normalize out example.
+x_house = np.array([1200, 3, 1, 40])
+x_house_norm = (x_house - X_mu) / X_sigma
+print(x_house_norm)
+x_house_predict = np.dot(x_house_norm, w_norm) + b_norm
+print(f" predicted price of a house with 1200 sqft, 3 bedrooms, 1 floor, 40 years old = ${x_house_predict*1000:0.0f}")
+
+# normalize the original features
+X_norm, X_mu, X_sigma = zscore_normalize_features(X_train)
+print(f"X_mu = {X_mu}, \nX_sigma = {X_sigma}")
+print(f"Peak to Peak range by column in Raw        X:{np.ptp(X_train,axis=0)}")   
+print(f"Peak to Peak range by column in Normalized X:{np.ptp(X_norm,axis=0)}")
+
+fig,ax=plt.subplots(1, 4, figsize=(12, 3))
+for i in range(len(ax)):
+    norm_plot(ax[i],X_train[:,i],)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("count");
+fig.suptitle("distribution of features before normalization")
+plt.show()
+fig,ax=plt.subplots(1,4,figsize=(12,3))
+for i in range(len(ax)):
+    norm_plot(ax[i],X_norm[:,i],)
+    ax[i].set_xlabel(X_features[i])
+ax[0].set_ylabel("count"); 
+fig.suptitle("distribution of features after normalization")
+
+plt.show()
+
+w_norm, b_norm, hist = run_gradient_descent(X_norm, y_train, 1000, 1.0e-1, )
+
+
+#predict target using normalized features
+m = X_norm.shape[0]
+yp = np.zeros(m)
+for i in range(m):
+    yp[i] = np.dot(X_norm[i], w_norm) + b_norm
+
+# plot predictions and targets versus original features    
+fig,ax=plt.subplots(1,4,figsize=(12, 3),sharey=True)
+for i in range(len(ax)):
+    ax[i].scatter(X_train[:,i],y_train, label = 'target')
+    ax[i].set_xlabel(X_features[i])
+    ax[i].scatter(X_train[:,i],yp,color="orange", label = 'predict')
+ax[0].set_ylabel("Price"); ax[0].legend();
+fig.suptitle("target versus prediction using z-score normalized model")
+plt.show()
+
+# First, normalize out example.
+x_house = np.array([1200, 3, 1, 40])
+x_house_norm = (x_house - X_mu) / X_sigma
+print(x_house_norm)
+x_house_predict = np.dot(x_house_norm, w_norm) + b_norm
+print(f" predicted price of a house with 1200 sqft, 3 bedrooms, 1 floor, 40 years old = ${x_house_predict*1000:0.0f}")
+
+plt_equal_scale(X_train, X_norm, y_train)
+
+print(f"EOF")
