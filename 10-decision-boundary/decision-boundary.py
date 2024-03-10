56d17e437764210c99df5cfe3a3ee5a77488505d
imran-dev100 <iagro7189@gmail.com>
2024-03-10 21:25:20 +0530

    adding decision boundary

---
+++ 10-decision-boundary/decision-boundary.py
@@ -0,0 +1,120 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Parameters
+    ----------
+    z : array_like
+        A scalar or numpy array of any size.
+
+    Returns
+    -------
+     g : array_like
+         sigmoid(z)
+    """
+    z = np.clip( z, -500, 500 )           # protect against overflow
+    g = 1.0/(1.0+np.exp(-z))
+
+    return g
+
+def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+    """ plots logistic data with two axis """
+    # Find Indices of Positive and Negative Examples
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  #work with 1D or 1D y vectors
+    neg = neg.reshape(-1,)
+
+    # Plot examples
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'red', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='o', s=s, label=neg_label, facecolors='none', edgecolors='blue', lw=3)
+    ax.legend(loc=loc)
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
+
+# Draws a threshold at 0.5
+def draw_vthresh(ax,x):
+    """ draws a threshold """
+    ylim = ax.get_ylim()
+    xlim = ax.get_xlim()
+    ax.fill_between([xlim[0], x], [ylim[1], ylim[1]], alpha=0.2, color='#193750')
+    ax.fill_between([x, xlim[1]], [ylim[1], ylim[1]], alpha=0.2, color='#840000')
+    ax.annotate("z >= 0", xy= [x,0.5], xycoords='data',
+                xytext=[30,5],textcoords='offset points')
+    d = FancyArrowPatch(
+        posA=(x, 0.5), posB=(x+3, 0.5), color='#840000',
+        arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+    )
+    ax.add_artist(d)
+    ax.annotate("z < 0", xy= [x,0.5], xycoords='data',
+                 xytext=[-50,5],textcoords='offset points', ha='left')
+    f = FancyArrowPatch(
+        posA=(x, 0.5), posB=(x-3, 0.5), color='#193750',
+        arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+    )
+    ax.add_artist(f)
+    
+# Input is an array. 
+input_array = np.array([1,2,3])
+exp_array = np.exp(input_array)
+
+print("Input to exp:", input_array)
+print("Output of exp:", exp_array)
+
+# Input is a single number
+input_val = 1  
+exp_val = np.exp(input_val)
+
+print("Input to exp:", input_val)
+print("Output of exp:", exp_val)
+
+
+
+X = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])
+y = np.array([0, 0, 0, 1, 1, 1]).reshape(-1,1) 
+
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X, y, ax)
+
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$')
+ax.set_xlabel('$x_0$')
+plt.show()
+
+# Plot sigmoid(z) over a range of values from -10 to 10
+z = np.arange(-10,11)
+
+fig,ax = plt.subplots(1,1,figsize=(5,3))
+# Plot z vs sigmoid(z)
+ax.plot(z, sigmoid(z), c="b")
+
+ax.set_title("Sigmoid function")
+ax.set_ylabel('sigmoid(z)')
+ax.set_xlabel('z')
+draw_vthresh(ax,0)
+
+
+# Choose values between 0 and 6
+x0 = np.arange(0,6)
+
+x1 = 3 - x0
+fig,ax = plt.subplots(1,1,figsize=(5,4))
+# Plot the decision boundary
+ax.plot(x0,x1, c="b")
+ax.axis([0, 4, 0, 3.5])
+
+# Fill the region below the line
+ax.fill_between(x0,x1, alpha=0.2)
+
+# Plot the original data
+plot_data(X,y,ax)
+ax.set_ylabel(r'$x_1$')
+ax.set_xlabel(r'$x_0$')
+plt.show()
