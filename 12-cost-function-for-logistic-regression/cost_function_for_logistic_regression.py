f173d0119ce5b90f0938b50abf6ac8d86b92ba73
imran-dev100 <iagro7189@gmail.com>
2024-03-12 09:09:57 +0530

    cost-function-for-logistic-regression

---
+++ 12-cost-function-for-logistic-regression/cost_function_for_logistic_regression.py
@@ -0,0 +1,115 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0')
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0'
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+
+def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+    """ plots logistic data with two axis """
+    # Find Indices of Positive and Negative Examples
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  #work with 1D or 1D y vectors
+    neg = neg.reshape(-1,)
+
+    # Plot examples
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'red', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='o', s=s, label=neg_label, facecolors='none', edgecolors=dlblue, lw=3)
+    ax.legend(loc=loc)
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Parameters
+    ----------
+    z : array_like
+        A scalar or numpy array of any size.
+
+    Returns
+    -------
+     g : array_like
+         sigmoid(z)
+    """
+    z = np.clip( z, -500, 500 )           # protect against overflow
+    g = 1.0/(1.0+np.exp(-z))
+
+    return g
+
+def compute_cost_logistic(X, y, w, b):
+    """
+    Computes cost
+
+    Args:
+      X (ndarray (m,n)): Data, m examples with n features
+      y (ndarray (m,)) : target values
+      w (ndarray (n,)) : model parameters  
+      b (scalar)       : model parameter
+      
+    Returns:
+      cost (scalar): cost
+    """
+
+    m = X.shape[0]
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i],w) + b
+        f_wb_i = sigmoid(z_i)
+        cost +=  -y[i]*np.log(f_wb_i) - (1-y[i])*np.log(1-f_wb_i)
+             
+    cost = cost / m
+    return cost
+
+X_train = np.array([[0.5, 1.5], [1,1], [1.5, 0.5], [3, 0.5], [2, 2], [1, 2.5]])  #(m,n)
+y_train = np.array([0, 0, 0, 1, 1, 1])                                           #(m,)
+
+
+fig,ax = plt.subplots(1,1,figsize=(4,4))
+plot_data(X_train, y_train, ax)
+
+# Set both axes to be from 0-4
+ax.axis([0, 4, 0, 3.5])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.show()
+
+w_tmp = np.array([1,1])
+b_tmp = -3
+print(compute_cost_logistic(X_train, y_train, w_tmp, b_tmp))
+
+
+# Choose values between 0 and 6
+x0 = np.arange(0,6)
+
+# Plot the two decision boundaries
+x1 = 3 - x0
+x1_other = 4 - x0
+
+fig,ax = plt.subplots(1, 1, figsize=(4,4))
+# Plot the decision boundary
+ax.plot(x0,x1, c=dlc["dlblue"], label="$b$=-3")
+ax.plot(x0,x1_other, c=dlc["dlmagenta"], label="$b$=-4")
+ax.axis([0, 4, 0, 4])
+
+# Plot the original data
+plot_data(X_train,y_train,ax)
+ax.axis([0, 4, 0, 4])
+ax.set_ylabel('$x_1$', fontsize=12)
+ax.set_xlabel('$x_0$', fontsize=12)
+plt.legend(loc="upper right")
+plt.title("Decision Boundary")
+plt.show()
+
+
+w_array1 = np.array([1,1])
+b_1 = -3
+w_array2 = np.array([1,1])
+b_2 = -4
+
+print("Cost for b = -3 : ", compute_cost_logistic(X_train, y_train, w_array1, b_1))
+print("Cost for b = -4 : ", compute_cost_logistic(X_train, y_train, w_array2, b_2))
